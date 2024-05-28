e2221abd7a9b2a2f9023c6f149b5d02092152ef8
itsmuiz <pvt.muiz1012@gmail.com>
2024-05-28 08:56:15 -0700

    completed

--- config.js
+++ config.js
@@ -1,33 +1,32 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // The length of sentences can be arbitrary. You can write ten sentences or twenty.
     // Try not to exceed 15 characters per sentence, as the display effect might not be good.
     texts: [
-        "For my", // Here, the last comma at the end of each sentence must be an English comma!! Very important!!
-        "beloved Purna",  // Same as above...
-        "Today is your birthday",
-        "We met a week ago",
+        "For my dear", // Here, the last comma at the end of each sentence must be an English comma!! Very important!!
+        "special someone",  // Same as above...
+        "Today is your day",
+        "We met long ago",
         "And it's already your birthday",
         "Let's make this day special",
         "With lots of love and joy",
         "May all your wishes come true",
         "Enjoy every moment",
         "And have a wonderful day",
-        "Happy Birthday, Purna!",
+        "Happy Birthday to you!",
     ],
     /**
      * imgs can be left empty, but if you want to fill it, it must follow the format below:
      * "Exactly the same as the above text" : "Image address, you can put the images in the imgs folder"
      * For example:
-     * "beloved Purna": "./imgs/purna.png"
+     * "special someone": "./imgs/someone.jpg"
      *
      * If you don't want an image, just comment out the line with double slashes at the beginning, 
-     * as in the example below, the image for "Today is your birthday" will not be displayed :)
+     * as in the example below, the image for "Today is your day" will not be displayed :)
      * Tip: It’s better to use square or near-square images for better visual effects.
      */
     imgs: {
-        "beloved Purna": "./imgs/purna.jpg",
-        // "Today is your birthday": "./imgs/birthday.jpg",
+        "special someone": "./imgs/someone.jpg",
+        // "Today is your day": "./imgs/birthday.jpg",
     },
     // Button text descriptions, the following are the default button texts in English. You can change them to your preferred text.
     desc: {
